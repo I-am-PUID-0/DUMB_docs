9b019ec13942f64140b6e2bbc8716b4cd95f63ce
hoivikaj <github@hoivik.co>
2026-01-03 10:36:59 -0500

    chore(ci): update devcontainer config

---
+++ .devcontainer/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+pip install --upgrade pip
+pip install poetry
+
+poetry config virtualenvs.create false
+poetry install --no-interaction --no-root
+
+exec zensical serve
